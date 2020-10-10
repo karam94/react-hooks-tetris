50336684477c3808ae1d4431ac7d86844c01a543
Karam Kabbara <contact@karam.io>
2020-10-10 13:17:31 +0100

    Added reset game and tetromino display with movement

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -5,6 +5,7 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
+import { createStage } from "../gameHelpers";
 import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
 
@@ -12,25 +13,60 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
-  console.log("re-render")
+  console.log("re-render");
+
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0, collided: false });
+  };
+
+  const startGame = () => {
+    // Reset the game
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    const leftArrow = 37;
+    const rightArrow = 39;
+    const downArrow = 40;
+
+    if (!gameOver) {
+      if (keyCode === leftArrow) {
+        movePlayer(-1);
+      } else if (keyCode === rightArrow) {
+        movePlayer(1);
+      } else if (keyCode === downArrow) {
+        dropPlayer();
+      }
+    }
+  };
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
           {gameOver ? (
             <Display gameOver text="Game Over" />
           ) : (
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
-import { getRandomTetromino } from "../tetrominos";
+import { useState, useCallback } from "react";
+import { STAGE_WIDTH } from "../gameHelpers";
+import { getRandomTetromino, TETROMINOS } from "../tetrominos";
 
 export const usePlayer = () => {
+  const emptyTetromino = TETROMINOS[0].shape;
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: getRandomTetromino().shape,
+    tetromino: emptyTetromino,
     collided: false,
   });
 
-  return [player];
+  const updatePlayerPos = ({ x, y, collided }) => {
+    // prev is the previous state obj of player
+    // modify the previous state obj and overwrite with new state obj
+    setPlayer((prev) => ({
+      ...prev,
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+      collided,
+    }));
+  };
+
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: getRandomTetromino().shape,
+      collided: false,
+    });
+  }, []);
+
+  return [player, updatePlayerPos, resetPlayer];
 };
